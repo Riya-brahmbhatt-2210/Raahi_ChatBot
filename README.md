8fdc021752f8fbc85c208f3626538d04fd28b5a9
djrdv <djrdv@LAPTOP-EC9RPF3Q>
2021-06-11 22:54:38 -0400

    Initial Commit

--- README.md
+++ README.md
@@ -6,16 +6,18 @@ To run:
 
 1. The first time run `npm install`
 2. Press ctrl-f5 while your focus is in one of the files that starts with a number and is all lower case.
+3. run `npm start` 
+4. go to browser and write "Localhost:(port name)"
+5. If you want to run through nodemon just type `nodemon run-script dev` instead of `npm start` 
 
-## Assignment 1
+## Assignment 1 Steps (Welcome To Raahi Restaurant)
 
-Create an order bot for your favourite food. You need to have at least 2 items on the menu unless your favourite food is pizza in which case you need to have 3 (I did pizza already). The menu items need to have size and one other attribute like toppings. You also need an up-sell item like drinks in the example.
-
-### Marking
-
-basic order for an item in a zip (65%)
-2nd or 3rd item (up to 10%)
-up-sell item other than drinks (up to 10%)
-estimated price or ability for someone in the store to see the orders with their phone (up to 15%)
-
-There is a brief [presentation here](EventsAndObjects.pdf).
+1. First of all on initial stage write "Hello or a greeting" in a chat and press enter
+2. It will provide you with menu. choose one of the item from the menu by replying with a number provided on side like 
+(1 or 2 or 3 or 4). if you input other than that it will show you the message that please select valid input.
+3. then it will ask you about the size of that specific item that you have choosen. reply with (Small or Medium or Large) other than    that will show you an error message.
+4. then it will ask you about toppings that you want to include in your item. give input with a string. if multiple items divide names with space and if it is a two word item use "-" (Hyphen) for that.
+5. After that it will ask you if you want to include a up-sell item "Muffin". I you say yes it will say a muffin in your order and if you say no it will not add.
+6. Next question for you will be if you want to add drinks or not? if yes what drink would you want to add? reply with yes and then name.
+7. after that it will ask you if you want to add another item to the order or not? if yes it will make sure again by asking you "are you sure?"
+8. if yes it will show the menu again and if no it will show you the overall order Summary and order total. It will also include pickup time with the order at last.
--- assignment1Shwarma.js
+++ assignment1Shwarma.js
@@ -2,51 +2,174 @@ const Order = require("./assignment1Order");
 
 const OrderState = Object.freeze({
     WELCOMING:   Symbol("welcoming"),
+    MENU:   Symbol("Menu"),
     SIZE:   Symbol("size"),
     TOPPINGS:   Symbol("toppings"),
-    DRINKS:  Symbol("drinks")
+    DRINKS:  Symbol("drinks"),
+    MUFFIN:  Symbol("Muffin"),
+    FINISH: Symbol("Finish"),
+    EXTRA: Symbol("Extra")
+});
+
+
+const ITEMPRICE = Object.freeze({
+    wrap:{
+        small:6,
+        Medium:8,
+        Large:10
+    },
+    burger:{
+        small:6,
+        Medium:8,
+        Large:10
+    },
+    pizza:{
+        small:6,
+        Medium:8,
+        Large:10
+    },
+    coldDrinks: 3,
+    Muffin:4
 });
 
 module.exports = class ShwarmaOrder extends Order{
     constructor(){
         super();
+        this.or="";
+        this.ch=0;
+        this.f=0;
+        this.price=0;
         this.stateCur = OrderState.WELCOMING;
         this.sSize = "";
         this.sToppings = "";
+        this.sMuffin = "";
         this.sDrinks = "";
         this.sItem = "shawarama";
+        this.staticStr="Order Summary: ";
+        this.FinalOrder="";
     }
     handleInput(sInput){
         let aReturn = [];
         switch(this.stateCur){
             case OrderState.WELCOMING:
+                if(this.f!=0){
+                    this.FinalOrder+=`${this.sSize} ${this.or} with `;
+                    this.FinalOrder+=`${this.sToppings}`; 
+                }
+                this.stateCur = OrderState.MENU;
+                aReturn.push("Welcome to Raahi Restuarant.");
+                aReturn.push("Choose From following Menu:");
+                aReturn.push("1. Wrap \n 2. Burger \n 3. Pizza \n 4. Exit");
+                break;
+            case OrderState.MENU:
+                if(sInput === "1"){
+                    this.or="Wrap";
+                    this.f++;
+                }
+                else if(sInput === "2"){
+                    this.or="Burger";
+                    this.f++;
+                }
+                else if(sInput === "3"){
+                    this.or="Pizza";
+                    this.f++;
+                }
+                else if(sInput === "4"){
+                    this.stateCur=OrderState.FINISH;
+                    this.ch=0;
+                    if(this.f>1){
+                        this.FinalOrder+=`\n${this.sSize} ${this.or} with `;
+                        this.FinalOrder+=`${this.sToppings}`; 
+                    }
+                    aReturn.push(this.staticStr);
+                    aReturn.push(this.FinalOrder);
+                    if(this.sDrinks)
+                        aReturn.push(this.sDrinks);
+                    if(this.sMuffin)
+                        aReturn.push("Muffin");
+                    aReturn.push(`And your Order total is $${this.price}`)
+                    let d = new Date(); 
+                    d.setMinutes(d.getMinutes() + 20);
+                    aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+                    return aReturn;
+                }
+                else{
+                    this.stateCur=OrderState.MENU;
+                    aReturn.push("Please Enter Valid Input!!");
+                    break;
+                }
                 this.stateCur = OrderState.SIZE;
-                aReturn.push("Welcome to Richard's Shawarma.");
-                aReturn.push("What size would you like?");
+                aReturn.push(`What size would you like of your ${this.or}? (small / Medium / Large)`);
                 break;
             case OrderState.SIZE:
+                if( sInput.toLowerCase()==="small")
+                    this.price+=ITEMPRICE.wrap.small;
+                else if( sInput.toLowerCase()==="medium")
+                    this.price+=ITEMPRICE.wrap.Medium;
+                else if( sInput.toLowerCase()==="large")
+                    this.price+=ITEMPRICE.wrap.Large;
+                else{
+                    this.stateCur=OrderState.SIZE;
+                    aReturn.push("Please Enter Valid Input!! (small / Medium / Large)");
+                    break;
+                }
                 this.stateCur = OrderState.TOPPINGS
                 this.sSize = sInput;
-                aReturn.push("What toppings would you like?");
+                aReturn.push(`What toppings would you like with your ${this.or}?`);
                 break;
             case OrderState.TOPPINGS:
-                this.stateCur = OrderState.DRINKS
+                this.stateCur = OrderState.MUFFIN
                 this.sToppings = sInput;
+                aReturn.push("Would you like Muffin with that?");
+                break;
+            case OrderState.MUFFIN:
+                this.stateCur = OrderState.DRINKS
+                if(sInput.toLowerCase()==="yes"){
+                    this.price+=ITEMPRICE.Muffin;
+                    this.sMuffin = sInput;
+                }
                 aReturn.push("Would you like drinks with that?");
                 break;
             case OrderState.DRINKS:
-                this.isDone(true);
+                this.stateCur = OrderState.EXTRA;
                 if(sInput.toLowerCase() != "no"){
-                    this.sDrinks = sInput;
+                    aReturn.push("what drink would you like to have? ");
+                    this.price+=ITEMPRICE.coldDrinks;
+                    break;
+                }    
+                this.stateCur = OrderState.FINISH;
+                aReturn.push("would you like to choose another item? ");
+                break;
+            case OrderState.EXTRA:
+                this.stateCur = OrderState.FINISH;
+                this.sDrinks += " "+ sInput;
+                aReturn.push("would you like to choose another item? ");
+                break;
+            case OrderState.FINISH:
+                if(sInput.toLowerCase() === "no"){
+                    this.stateCur=OrderState.FINISH;
+                    this.ch=1;
+                    this.FinalOrder+=`\n${this.sSize} ${this.or} with `;
+                    this.FinalOrder+=`${this.sToppings}`; 
                 }
-                aReturn.push("Thank-you for your order of");
-                aReturn.push(`${this.sSize} ${this.sItem} with ${this.sToppings}`);
-                if(this.sDrinks){
-                    aReturn.push(this.sDrinks);
+                else if(sInput.toLowerCase() === "yes"){
+                    this.stateCur=OrderState.WELCOMING;
+                    aReturn.push("Are you sure? (Yes / No)");
+                    this.ch=0;
                 }
-                let d = new Date(); 
-                d.setMinutes(d.getMinutes() + 20);
-                aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+
+                if(this.ch===1){
+                    aReturn.push(this.staticStr);
+                    aReturn.push(this.FinalOrder);
+                    if(this.sDrinks)
+                        aReturn.push(this.sDrinks);
+                    if(this.sMuffin)
+                        aReturn.push("Muffin");
+                    aReturn.push(`And your Order total is $${this.price}`)
+                    let d = new Date(); 
+                    d.setMinutes(d.getMinutes() + 20);
+                    aReturn.push(`Please pick it up at ${d.toTimeString()}`);
+                }
                 break;
         }
         return aReturn;
